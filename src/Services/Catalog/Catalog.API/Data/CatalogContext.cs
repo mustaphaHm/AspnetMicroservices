56799777e1eaedd2bfa6033a691ece7e2e9b92f4
MustaphaHm <mustapha.houmam@gmail.com>
2023-08-03 17:01:37 +0100

    Adding product class and catalog context

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSetting:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSetting:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSetting:CollectionName"));
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
